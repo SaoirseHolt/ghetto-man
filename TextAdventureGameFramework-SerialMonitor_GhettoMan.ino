c714330e183f35ca2a617970c57624de834900c3
Saoirse Holt <holt.saoirse@gmail.com>
2020-04-30 23:06:35 -0700

    Update TextAdventureGameFramework-SerialMonitor_GhettoMan.ino
    In the process of reworking how the game instructions are stored. The plan is to migrate to SD card storage, and just use the program memory to hold an interpreter for the eventual txt file. Also did a bit of cleanup.

--- TextAdventureGameFramework-SerialMonitor_GhettoMan.ino
+++ TextAdventureGameFramework-SerialMonitor_GhettoMan.ino
@@ -1,4 +1,4 @@
-#include <openGLCD.h>
+specialClrScreen#include <openGLCD.h>
 #include <openGLCD_Buildinfo.h>
 #include <openGLCD_Config.h>
 #include <stdarg.h>
@@ -7,34 +7,41 @@
 #include "bitmaps/allBitmaps.h"
 
 const arr_len(x) = (sizeof(x) / sizeof(*x));
-const int A_Button = 1;
-const int B_Button = 2;
-const int C_Button = 3;
-const int R_Bumper = 4;
-const int L_Bumper = 5;
-int Button = 0; //Stores what button has been pushed to correspond with an action.
+const byte A_Button = 1;
+const byte B_Button = 2;
+const byte C_Button = 3;
+const byte R_Bumper = 4;
+const byte L_Bumper = 5;
+byte Button = 0; //Stores what button has been pushed to correspond with an action.
 String selOpt; //Stores lineA, lineB, or lineC depending on which button is pressed when prompted.
 String incString; //Used by buttonPress() to store data recieved through Serial input.
 int Health = 100; //Player's health.
-int WeaponUsed = 0; //Stores the current weapon that the player is holding.
-int contPages = 1; //Modified by contextOptions() to store number of pages of active context options. Each page will reflect 3 or fewer options.
-int currentPage = 1; //Modified by contextOptions() to store active page.
+int maxHealth = 100; //Max player health.
+byte weaponUsed = 0; //Stores the current weapon that the player is holding.
+byte contPages = 1; //Modified by contextOptions() to store number of pages of active context options. Each page will reflect 3 or fewer options.
+byte currentPage = 1; //Modified by contextOptions() to store active page.
 String lineA = ""; //Modified by contextOptions to store the up to 3 active context options.
 String lineB = "";
 String lineC = "";
-boolean SpecialClrScreen = 0;
-boolean Item[10]; //Array that stores the items you've collected.
-const int NUM_ITEMS = arr_len(Item);
-boolean Weapon[10]; //Array that stores the weapons you've collected.
-const int NUM_WEAPS = arr_len(Weapon);
-int curStage; //Variable that stores the current scene. Used by setStage() and changeStage() to load the appropriate scene.
+boolean specialClrScreen = 0;
+boolean stageFlag[20][3]; //2D array that stores contextual flags for each stage. The first brackets refer to the number of stages, so -------NEED TO REVISE HOW GAME DATA IS STORED, COME BACK TO THIS
+boolean Inventory[10]; //Array that stores the items you've collected.
+const byte NUM_ITEMS = arr_len(Inventory);
+boolean weaponBag[10]; //Array that stores the weapons you've collected.
+const byte NUM_WEAPS = arr_len(weaponBag);
+const char TXT_FILE_SYNTAX[][11] = ("", "Prt", "if", "elseif", "else", "conOp", "checkFlag", "setFlag", "setStage", "getItem", "loseItem", "getWeap", "loseWeap", "combat", "heal", "damage");
+const byte NUM_TXT_FILE_SYNTAX = arr_len(TXT_FILE_SYNTAX);
+byte curStage; //Variable that stores the current scene. Used by setStage() and changeStage() to load the appropriate scene.
 boolean gameLoop; //Variable that causes the changeStage() function to loop inside adventureGame(). Set value to 0 to return to mainMenu().
 const boolean debugMode = 0; //Setting this value to 1 enables "debug mode" (Serial monitor input/output).
-
 
 gText fullTextarea = gText(0, 0, GLCD.Right, GLCD.Bottom, SystemFont5x7)
 gText eventTextarea = gText(0, 0, GLCD.Right, GLCD.CenterY-1, SystemFont5x7);
 gText actionTextarea = gText(0, GLCD.CenterY, GLCD.Right, GLCD.Bottom, SystemFont5x7);
+
+
+byte testInterpreter(int p, String BufP, String BufQ = "", String BufR = "") //Prototype (needed in order to define a default case)
+void Prt(String printText, byte printRegion = 1, boolean newLine = 1) //Prototype
 
 void setup()
 {
@@ -59,11 +66,11 @@ void ClearScreen(boolean L)
   }
   else if(L==1){ //Draws a box around the outside of the screen, for information-only scenes. INCOMPLETE
 
-    SpecialClrScreen = 1;
+    specialClrScreen = 1;
   }
 }
 
-void Prt(String printText, int printRegion = 1, boolean newLine = 1) //Print function. Should be called whenever text is to be printed. First parameter is text to print, second paremeter represents where on the GLCD to print (0 = fullscreen, 1 = top half, 2 = bottom half). Third parameter should be set to 0 to prevent a newline.
+void Prt(String printText, byte printRegion, boolean newLine) //Print function. Should be called whenever text is to be printed. First parameter is text to print, second parameter represents where on the GLCD to print (0 = fullscreen, 1 = top half, 2 = bottom half). Third parameter should be set to 0 to prevent a newline.
 {
   if (newLine == 0) {
     switch (printRegion) {
@@ -131,7 +138,7 @@ void mainMenu() //Main menu function.
   else if (selOpt == "Credits") {credits();}
 }
 
-void buttonPress(int N) //Function to assign button value to $Button. The argument specifies how many inputs we expect, as detailed below. Use buttonPress(0) if you don't care what button is pressed, but it cannot be triggered by L or R (4 or 5).
+void buttonPress(byte N) //Function to assign button value to $Button. The argument specifies how many inputs we expect, as detailed below. Use buttonPress(0) if you don't care what button is pressed, but it cannot be triggered by L or R (4 or 5).
 {
 //  while(digitalRead(A_Button == LOW) && digitalRead(B_Button == LOW) && digitalRead(C_Button == LOW) && digitalRead(R_Bumper == LOW) && digitalRead(L_Bumper == LOW)) {};
 //  if (digitalRead(A_Button == HIGH)) {Button = 1;}
@@ -209,7 +216,7 @@ void contextOptions(char* charArray, ...) //Accepts any number of arguments, it 
 {
   va_list argptr;
   char* cA = charArray;
-  int C = 0; //Reflects the number of arguments passed. (A value of 3 means 3 arguments, and so on.)
+  byte C = 0; //Reflects the number of arguments passed. (A value of 3 means 3 arguments, and so on.)
   String strBuf = "";
 
   va_start(argptr, charArray);
@@ -221,16 +228,16 @@ void contextOptions(char* charArray, ...) //Accepts any number of arguments, it 
   va_end(argptr);
 
   String contextOptions[C+1];
-  for (int i=0; i<C; i++) { //Reads strBuf until it finds a comma, indicating a new element. Stores that element as a key in contextOptions, then removes it from strBuf.
-    int commaLoc = strBuf.indexOf(',');
+  for (byte i=0; i<C; i++) { //Reads strBuf until it finds a comma, indicating a new element. Stores that element as a key in contextOptions, then removes it from strBuf.
+    byte commaLoc = strBuf.indexOf(',');
     contextOptions[i] = strBuf.substring(0, commaLoc);
     strBuf.remove(0, commaLoc+1);
   }
 
   contPages = C / 3; //Uses the variable C to determine number of context option pages. Each page consist of 3 or fewer options.
-  int remainder = C % 3;
+  byte remainder = C % 3;
   if (remainder > 0) {contPages++;}
-  int pageContRange = currentPage * 3; //Writes context options within the range of the current page to lineA, lineB, and lineC.
+  byte pageContRange = currentPage * 3; //Writes context options within the range of the current page to lineA, lineB, and lineC.
   if (pageContRange - 2 <= C) {
     lineA = contextOptions[pageContRange - 3];
     if (pageContRange - 1 <= C) {
@@ -301,31 +308,45 @@ void contextOptions(char* charArray, ...) //Accepts any number of arguments, it 
 //  else {buttonPressNested();}
 //}
 
-void recieveItem(int v)
+void Item(byte x, boolean y)
 {
-  Item[v] = 1;
+  Inventory[x] = y;
 }
 
-void recieveWeapon(int w)
+void Weapon(byte x, boolean y)
 {
-  Weapon[w] = 1;
+  weaponBag[x] = y;
+}
+
+// INCOMPLETE
+//    |
+//    V
+byte Damage(byte x, boolean y)
+{
+
+  if (y) {Health = Health + x; }
+  else {Health = Health - x}
+  Health = constrain(Health, 0, 100);
+  if (Health == 0) {} //Implement YOU ARE DEAD functionality!!!
+
+  return
 }
 
 void clearItems()
 {
-  for (int i = 0; i < NUM_ITEMS; i++) {
+  for (byte i = 0; i < NUM_ITEMS; i++) {
     Item[i] = 0;
   }
 }
 
 void clearWeapons()
 {
-  for (int i = 0; i < NUM_WEAPS; i++) {
-    Weapon[i] = 0;
+  for (byte i = 0; i < NUM_WEAPS; i++) {
+    weaponBag[i] = 0;
   }
 }
 
-void setStage(int n) //Function to set which is the current scene. Sets all other stages to 0, then sets the current one to 1. Sets currentPage to 1 when called so we always start on the first page of a new scene.
+void setStage(byte n) //Function to set which is the current scene. Sets all other stages to 0, then sets the current one to 1. Sets currentPage to 1 when called so we always start on the first page of a new scene.
 {
   curStage = n;
   currentPage = 1;
@@ -333,7 +354,7 @@ void setStage(int n) //Function to set which is the current scene. Sets all othe
 
 void changeStage() //Function to load the proper scene. Goes through all the possible stages until it finds the one that is set to 1 (the scene to load), and then loads it.
 {
-//  if (SpecialClrScreen == 1) {SpecialClrScreen = 0;}
+//  if (specialClrScreen == 1) {specialClrScreen = 0;}
 //  else {ClearScreen(0);}
 
 //  Serial.println(curStage); //For debugging, prints the current stage to the screen.
@@ -405,8 +426,113 @@ void credits() //Credits function for main menu.
   buttonPress(0);
 }
 
-//From here on, all functions represent a form of a scene.
+/*  stageFlag[][] BREAKDOWN
 
+0-0: Tray
+0-1: Shiv
+1-0:
+
+*/
+
+//From here on, all functions represent a form of a stage.
+
+byte testInterpreter(byte p, char* BufQ, char* BufR, char* BufS, char* BufT, char* BufU) //The goal is to create a function that can interpret data read in from an SD card into operable commands. Wish me luck... (WIP)
+{
+    byte q, r, s, t, u;
+    for (byte i = 0, i < NUM_TXT_FILE_SYNTAX, i++) {
+      if (BufQ == TXT_FILE_SYNTAX[i]) {q = i;}
+      if (BufR == TXT_FILE_SYNTAX[i]) {r = i;}
+      if (BufS == TXT_FILE_SYNTAX[i]) {s = i;}
+      if (BufT == TXT_FILE_SYNTAX[i]) {t = i;}
+      if (BufU == TXT_FILE_SYNTAX[i]) {u = i;}
+    }
+//Cases 0 - 15("", "Prt", "if", "elseif", "else", "conOp", "checkFlag", "setFlag", "setStage", "getItem", "loseItem", "getWeap", "loseWeap", "combat", "heal", "damage")
+//            (0,    1,    2,      3,       4,       5,        6,           7,         8,          9,         10,        11,         12,        13,     14,     15)
+    switch (q) { //This block interprets the first parameter passed to the interpreter.
+      case 1: //Prt
+        Prt(BufR)
+      break;
+      case 2: //if statement
+        switch (r) {
+          case 6: //checkFlag
+            //BufS -> do something to break it out into 2 separate byte numbers that can be used for checking stageFlags
+            if (stageFlag[x][y]) {
+              switch (t) {
+                case 5: //conOp
+                  //
+                break;
+                case 7: //setFlag
+                  //
+                break;
+                case 8: //setStage
+                  //
+                break;
+                case 9: //getItem
+                  //
+                break;
+                case 10: //loseItem
+                  //
+                break;
+                case 11: //getWeap
+                  //
+                break;
+                case 12: //loseWeap
+                  //
+                break;
+                case 13: //combat
+                  //
+                break;
+                case 14: //heal
+                  //
+                break;
+                case 15: //damage
+                  //
+                break;
+                default:
+                  //error state
+                break;}}
+          break;
+          default:
+            //error state
+          break;}
+      break;
+      case 3: //else if statement
+      //
+      break;
+      case 4: //else statement
+      //
+      break;
+      case 5:
+      //
+      break;
+      default:
+      //error state
+      break;
+    }
+    switch (r) { //This block interprets the second parameter, and passes on if there was no 2nd parameter.
+      case 0:
+      //There was no 2nd parameter
+      break;
+      default:
+      //error state
+      break;
+    }
+
+    p++;
+    return p;
+}
+
+void SDParser() // Reads data from SD card and writes to an array of strings (a 2d array). Also counts the number of elements in the array. This info is passed piece by piece to the Interpreter function to be executed as code.
+{
+  byte SDBufPos;
+  byte SDBufLength;
+  //Do something to read the txt file into arrays
+  while (SDBufPos < SDBufLength) {
+    SDBufPos = testInterpreter(SDBufPos, SDBuffer[SDBufPos], SDBuffer[SDBufPos+1], SDBuffer[SDBufPos+2], SDBuffer[SDBufPos+3], SDBuffer[SDBufPos+4]); //Need to revise this. The idea is that we will pass up to 4 elements of our array at a time (this is the upper limit to how many "things" we can parse at once: current command, possible argument, possible second argument or command, etc.)
+  }
+}
+
+//Revise all these stage functions. They will eventually be migrated to a separate txt file.
 void Prison() //Stage 0
 {
   Prt("You observe a multitude of objects adorning your cell.");
